e7eea2408c0a3622243648a18cfddf62c9d17b4e
MOreshhhhh <156440721+MOreshhhhh@users.noreply.github.com>
2025-12-25 22:15:36 +0530

    Added Variable uses code

---
+++ variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String args[]){
+        int age = 22;
+        String name = "Moresh";
+        double cgpa = 9.9;
+        boolean val = true;
+
+        System.out.println("My name is " + name + "\nAge is " + age + "\nCGPA is " + cgpa + "\nLearning java  " + val);
+    }
+}
